b9ca35f59517b07c53f3ee21c284fed9d7de9b40
gorolykmaxim <streetbattle52@gmail.com>
2019-02-23 12:39:55 +0200

    This time it should actually work

--- initialize-qbittorrent.py
+++ initialize-qbittorrent.py
@@ -4,5 +4,4 @@ import subprocess
 
 qbittorrent = subprocess.Popen(['qbittorrent-nox'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 qbittorrent.stdin.write(b"y\n")
-qbittorrent.stdout.readline(1)
-qbittorrent.kill()
+qbittorrent.terminate()
