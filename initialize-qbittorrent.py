506b5e7655fd99d66ccafdf1574fa3d339ff76eb
gorolykmaxim <streetbattle52@gmail.com>
2019-02-23 12:36:42 +0200

    Make initialize-qbittorrent.py work

--- initialize-qbittorrent.py
+++ initialize-qbittorrent.py
@@ -3,8 +3,6 @@
 import subprocess
 
 qbittorrent = subprocess.Popen(['qbittorrent-nox'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-qbittorrent.stdin.write("y\n")
-success_message = 'qBittorrent is successfully listening'
-for line in qbittorrent.stdout:
-    if success_message in line:
-        qbittorrent.kill()
+qbittorrent.stdin.write(b"y\n")
+qbittorrent.stdout.readline(1)
+qbittorrent.kill()
