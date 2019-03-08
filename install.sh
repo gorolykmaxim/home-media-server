569b2ccf82df4bb0e292199bb29b56d08e540c8b
gorolykmaxim <streetbattle52@gmail.com>
2019-03-08 15:46:08 +0200

    Split docker-compose.yml into a base configuration and a production one
    This allows us to run the base configuration (containers with minimal
    required configuration) on our development environments. And the part
    that is kinda pain-in-the-arse to configure is only run in production
    (were install.sh is configuring it automatically).

--- install.sh
+++ install.sh
@@ -15,11 +15,11 @@ mkdir ${MEDIA_FOLDER_PATH}
 USER_NAME=`whoami`
 USER_ID=`id -u ${USER_NAME}`
 GROUP_ID=`id -g ${USER_NAME}`
-cp docker-compose.template.yml docker-compose.yml
-sed -i "s/{userId}/${USER_ID}/g" docker-compose.yml
-sed -i "s/{userGroupId}/${GROUP_ID}/g" docker-compose.yml
-sed -i "s@{configFolderPath}@${ROOT_FOLDER_PATH}@g" docker-compose.yml
-sed -i "s@{mediaFolderPath}@${MEDIA_FOLDER_PATH}@g" docker-compose.yml
+cp docker-compose.prod.template.yml docker-compose.prod.yml
+sed -i "s/{userId}/${USER_ID}/g" docker-compose.prod.yml
+sed -i "s/{userGroupId}/${GROUP_ID}/g" docker-compose.prod.yml
+sed -i "s@{configFolderPath}@${ROOT_FOLDER_PATH}@g" docker-compose.prod.yml
+sed -i "s@{mediaFolderPath}@${MEDIA_FOLDER_PATH}@g" docker-compose.prod.yml
 
 # Deploy containers
-sudo docker-compose up -d
+sudo docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
