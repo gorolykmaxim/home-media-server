5aee1130ee94425eada2e5624c64d0307385dfe4
gorolykmaxim <streetbattle52@gmail.com>
2019-03-27 10:18:00 +0200

    Create separate script for docker installation update
    update.sh deploys necessary docker containers and does not try
    to install docker.

--- install.sh
+++ install.sh
@@ -5,21 +5,4 @@ sudo apt install -y docker.io docker-compose
 sudo systemctl enable docker
 sudo systemctl start docker
 
-# Prepare directories for container mounting
-ROOT_FOLDER_PATH=`readlink -f ~/Downloads`
-MEDIA_FOLDER_PATH=${ROOT_FOLDER_PATH}/media
-mkdir ${ROOT_FOLDER_PATH}
-mkdir ${MEDIA_FOLDER_PATH}
-
-# Create docker-compose.yml from template
-USER_NAME=`whoami`
-USER_ID=`id -u ${USER_NAME}`
-GROUP_ID=`id -g ${USER_NAME}`
-cp docker-compose.prod.template.yml docker-compose.prod.yml
-sed -i "s/{userId}/${USER_ID}/g" docker-compose.prod.yml
-sed -i "s/{userGroupId}/${GROUP_ID}/g" docker-compose.prod.yml
-sed -i "s@{configFolderPath}@${ROOT_FOLDER_PATH}@g" docker-compose.prod.yml
-sed -i "s@{mediaFolderPath}@${MEDIA_FOLDER_PATH}@g" docker-compose.prod.yml
-
-# Deploy containers
-sudo docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
+./update.sh
