0f7f0ab433ca6b87fee848e3089fcd8f74d5ed7a
gorolykmaxim <streetbattle52@gmail.com>
2019-03-30 15:47:47 +0200

    Improve plex-listener logging

--- plex-listener/episode-view-event-proxy.py
+++ plex-listener/episode-view-event-proxy.py
@@ -7,5 +7,7 @@ matcher = re.compile(r"\'(.*)\'( -)? got played")
 url = 'http://home-media-app/api/v1/episode/view'
 for line in sys.stdin:
     result = matcher.match(line)
+    print('Matching: ', line)
     if result:
+        print('Found match: ', result.group(1))
         requests.post(url, data=json.dumps({'episodeName': result.group(1)}))
