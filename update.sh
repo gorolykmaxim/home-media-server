3cc91e240be92c4d2bb6bfa6ef161074f15da9c8
gorolykmaxim <streetbattle52@gmail.com>
2019-03-30 15:35:54 +0200

    Fix update.sh

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 # Prepare directories for container mounting
 ROOT_FOLDER_PATH=`readlink -f ~/Downloads`
 MEDIA_FOLDER_PATH=${ROOT_FOLDER_PATH}/media
-MEDIA_LOGS_FOLDER_PATH=${ROOT_FOLDER_PATH/Library/Application\ Support/Plex\ Media\ Server/Logs
+MEDIA_LOGS_FOLDER_PATH="${ROOT_FOLDER_PATH}/Library/Application Support/Plex Media Server/Logs"
 mkdir ${ROOT_FOLDER_PATH}
 mkdir ${MEDIA_FOLDER_PATH}
 
@@ -16,7 +16,7 @@ sed -i "s/{userId}/${USER_ID}/g" docker-compose.prod.yml
 sed -i "s/{userGroupId}/${GROUP_ID}/g" docker-compose.prod.yml
 sed -i "s@{configFolderPath}@${ROOT_FOLDER_PATH}@g" docker-compose.prod.yml
 sed -i "s@{mediaFolderPath}@${MEDIA_FOLDER_PATH}@g" docker-compose.prod.yml
-set -i "s@{mediaLogsFolderPath}@${MEDIA_LOGS_FOLDER_PATH}@g" docker-compose.prod.yml
+sed -i "s@{mediaLogsFolderPath}@${MEDIA_LOGS_FOLDER_PATH}@g" docker-compose.prod.yml
 
 # Deploy containers
 sudo docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
