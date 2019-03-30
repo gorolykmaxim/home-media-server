763e287d5e64de20bd2786bde0588b6a2fd1f960
gorolykmaxim <streetbattle52@gmail.com>
2019-03-30 16:30:37 +0200

    Fix regexp

--- plex-listener/episode-view-event-proxy.py
+++ plex-listener/episode-view-event-proxy.py
@@ -3,7 +3,7 @@ import json
 import re
 import sys
 
-matcher = re.compile(r"\'(.*)\'( -)? got played")
+matcher = re.compile(r".*\'(.*)\'( -)? got played.*")
 url = 'http://home-media-app/api/v1/episode/view'
 for line in sys.stdin:
     result = matcher.match(line)
