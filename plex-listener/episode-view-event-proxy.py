def98beddc44c88faeebf7c4be335202aa640dd2
gorolykmaxim <streetbattle52@gmail.com>
2019-04-01 09:46:55 +0300

    Move all configurable values of plex-listener into environment variables

--- plex-listener/episode-view-event-proxy.py
+++ plex-listener/episode-view-event-proxy.py
@@ -1,13 +1,14 @@
+import os
 import re
 import sys
 
 import requests
 
-matcher = re.compile(r".*\'(.*)\'( -)? got played.*")
-url = 'http://home-media-app:8080/api/v1/episode/view'
+matcher = re.compile(os.environ.get("MESSAGE_REGEXP"))
+url = os.environ.get("HOME_MEDIA_APP_URL")
 for line in sys.stdin:
     result = matcher.match(line)
     print('Matching: ', line)
     if result:
         print('Found match: ', result.group(1))
-        requests.post(url, json={'episodeName': result.group(1)})
+        requests.post(url, json={os.environ.get("ATTRIBUTE_NAME"): result.group(1)})
