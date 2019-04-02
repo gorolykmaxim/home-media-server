92a98621516cf3c68777900d4d4f4775138ae401
gorolykmaxim <streetbattle52@gmail.com>
2019-04-03 00:49:05 +0300

    Detect plex log rotation with 'tail -F' instead of 'tail -f'

--- plex-listener/listen.sh
+++ plex-listener/listen.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-tail -f /plex/logs/Plex\ Media\ Server.log | python3 episode-view-event-proxy.py
+tail -F /plex/logs/Plex\ Media\ Server.log | python3 episode-view-event-proxy.py
